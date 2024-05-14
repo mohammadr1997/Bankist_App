9146a44efeb42b7db7d4d61da8746caa146dee53
Mohammad-Rezaee <webnerd2024@gmail.com>
2024-05-14 21:01:00 +0330

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const btnEl=document.querySelector('.btn');
+const paragraphEl=document.querySelector('.paragraph')
+ btnEl.addEventListener('click',()=>{
+    paragraphEl.classList.toggle('hidden')
+ })
